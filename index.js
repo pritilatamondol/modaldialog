2d475ca9da0271a90ee35c92852cef924bc70790
pritim <pritim@naehas.com>
2020-09-16 23:13:05 -0700

    Third commit

--- index.js
+++ index.js
@@ -184,4 +184,4 @@ const createModalFooter = (params) => {
     return modalFooter + '</div>';
   };
 
-  module.exports.modaldialog = createModalDialog;
+  module.exports.createModalDialog = naehasdialog;
